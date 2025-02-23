892b012d3c7f9b928d3e1ef192a2c1abb7e8b775
thatabdullah <443014855@sm.imamu.edu.sa>
2025-02-24 01:52:09 +0300

    localizing admin panel + configured nginx

---
+++ muzawed/config/filament-language-switch.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'locales' => [
+        'en' => 'English',
+        'ar' => 'العربية',
+    ],
+    
+    
+     //Whether or not to show the language switch button
+     
+    'show' => true,
+
+    
+    //Whether to show the language full name or just the abbreviation
+     
+    'label_type' => 'full', // Options: full, abbreviation
+
+    
+    // Which side the language switch button should be displayed
+     
+    'position' => 'top-right', // Options: top-right, top-left
+];
---
+++ muzawed/lang/ar/filament.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'direction' => 'rtl',
+    
+    'fields' => [
+        'select' => [
+            'placeholder' => 'اختر خيارًا',
+        ],
+    ],
+    
+    'pagination' => [
+        'label' => 'التنقل بين الصفحات',
+        'overview' => 'عرض :first إلى :last من :total النتائج',
+        'previous' => 'السابق',
+        'next' => 'التالي',
+    ],
+
+    'buttons' => [
+        'submit' => 'إرسال',
+        'cancel' => 'إلغاء',
+        'save' => 'حفظ',
+        'create' => 'إنشاء',
+        'delete' => 'حذف',
+    ],
+];
